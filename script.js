7fee8b5bd6cceb1b71f5d1770c8a0dfad6aa062e
Arseniy <senya.s408@gmail.com>
2020-06-25 12:41:17 +0300

    added score counter
    added working score counter

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const word = document.getElementById('word');
 const button = document.getElementById('button');
 const input = document.getElementById('input');
 const settime = document.getElementById('settime');
+const score_counter = document.getElementById('score-counter');
 const words = ['typing','youtube','word','minecraft','skyrim','programming','sally','sam','hand','student','head',
 'pank','rock','bird','rain','terrain','school','study','book','face','work','facebook','book','ukraine',
 'sound','paragon','overwatch','egor','letov','bald','john','sense','razer','mouse','keyboard','quite',
@@ -20,26 +21,28 @@ let score = 0;
 let time = 60;
 
 button.addEventListener('click', function(){
-
-word.innerHTML = words[Math.floor(Math.random() * words.length)];
-let Interval = setInterval(function(){
-	if (time > 0) {
-		time--;
-		time_counter.innerHTML = `${time.toString()} s`;
-		input.oninput = function(){
-			if (input.value === word.innerHTML) {
-			word.innerHTML = words[Math.floor(Math.random() * words.length)];
-			input.value = "";
-			score += 1;
-			}
-		}
-	}else if(time === 0) {
-		time_counter.innerHTML = "60 s";
-		time = 60;
-		alert('Your score is ' + score);
-		word.innerHTML = "";
-		clearInterval(Interval);
-	}
-},1000);
-
+    score_counter.innerHTML = '0';
+    word.innerHTML = words[Math.floor(Math.random() * words.length)];
+    let Interval = setInterval(function(){
+	    if (time > 0) {
+		    time--;
+		    time_counter.innerHTML = `${time.toString()} s`;
+		    input.oninput = function(){
+		        if (input.value === word.innerHTML) {
+			        word.innerHTML = words[Math.floor(Math.random() * words.length)];
+			        input.value = "";
+                    score += 1;
+                    score_counter.innerHTML = score;
+			    }
+		    }
+	    }else if(time === 0) {
+            alert('Your score is ' + score);
+            score_counter.innerHTML = '0';
+		    time_counter.innerHTML = "60 s";
+            time = 60;
+            score = 0;
+		    word.innerHTML = "";
+		    clearInterval(Interval);
+	    }
+    },1000);
 });
